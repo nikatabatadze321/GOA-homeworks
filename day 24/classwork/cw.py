cd48506be82f9b4a8531821406519fc287c465d9
nikatabatadze321 <tabatadzen34@gmail.com>
2024-12-23 23:04:36 +0400

    commit

---
+++ day 24/classwork/cw.py
@@ -0,0 +1,8 @@
+a=input("Enter the name : ")
+b=input("Enter the password : ")
+while a != ("nika") and b != ("nika123") : 
+    print ("your name and password is incorrect")
+    a=input("please enter name again")
+    b=input("please enter password again")
+
+print("welcome")
