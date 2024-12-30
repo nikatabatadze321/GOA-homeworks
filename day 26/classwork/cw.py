075dfc62ad902e4907091c842f46a9eddd5bdd41
nikatabatadze321 <tabatadzen34@gmail.com>
2024-12-30 17:08:16 +0400

    second commit

---
+++ day 26/classwork/cw.py
@@ -0,0 +1,7 @@
+my_list = ["mercedees",["bmw"],["ferrari"],["toyota"],["rols royce"],["dodge"],["honda"],["lqmborghini"],["phagani"],["mclaren"]]
+print(my_list[1:5])
+print(my_list[0:6])
+print(my_list[5:-1])
+print(my_list[8])
+print(my_list[10])
+print(my_list[0:10])
