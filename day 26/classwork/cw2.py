164554c5aeae8c6ac08ebc1edd81dc0f1987b12c
nikatabatadze321 <tabatadzen34@gmail.com>
2025-01-13 22:04:39 +0400

    commit

---
+++ day 26/classwork/cw2.py
@@ -0,0 +1,11 @@
+my_list = ["dog",["cat"],["elephant"],["tiger"],["lion"],["wolf"]]
+my_list.pop(3)  
+my_list.pop(1)
+print (my_list)
+
+
+#2
+
+my_list= ["apple",["bannana"],["grapes"],["orange"],["lemon"]]
+my_list[1]=["watermalon"]
+print (my_list)
