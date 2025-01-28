6ed7c4dd6cccb889b5ce395661e4cdfa26933bd8
nikatabatadze321 <tabatadzen34@gmail.com>
2025-01-28 20:11:23 +0400

    commit

---
+++ day 29/classwork/cw.py
@@ -0,0 +1,6 @@
+num = [17,20,25,19,82,12,74,15,30,71]
+for i in range (len(num)):
+    if num[i] % 5 == 0:
+        print (str(num[i])+"xutis jeradi")
+    if num[i]% 3 == 0:
+        print (str(num[i])+"samis jeradi ")
