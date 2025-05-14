8d40a51626685124c27fe09ed8cd4e61325a90f9
nikatabatadze321 <tabatadzen34@gmail.com>
2025-05-14 23:33:31 +0400

    commit

---
+++ day 55/classwork/cw3.py
@@ -0,0 +1,20 @@
+word="hEllo worLd"
+print(word.upper())#გადააქცევს ყველა ასოს დიდად
+print(word.lower())#გადააქცევს ყვლა ასოს პატარად
+print(word.capitalize())#მხოლოდ პირელ ასოს ადიდებს
+new=word.lower()#ახალი ცვლადი რომ გადაიცეს პატარა ასოებით რათა საპოვნელად ადვილი იყოს
+print(new.find("e"))#აქ უკვე ვპოულობთ მერამდენე ინდექსზეა ასო რომელიც გვაინტერესებს
+
+another=("WjaJKJ Jsda")
+print(another.upper())
+print(another.lower())
+print(another.capitalize())
+another_1=another.lower()
+print(another_1.find("a"))
+
+one_more=("jna KMMKDA Ansd kmda")
+print(one_more.upper())
+print(one_more.lower())
+print(one_more.capitalize())
+xoda=one_more.lower()
+print(xoda.find("a"))
